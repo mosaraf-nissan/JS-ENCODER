fecab1b94a973436a3ad34dcaac0e1fd395aa0d6
Nissan1998 <jnnissanm@gmail.com>
2023-11-12 14:14:36 +0600

    aboutJsEncoderSection

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero";
 import HeroFooter from "../../components/HeroFooter";
+import AboutUs from "../AboutUs";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <HeroFooter />
+      <AboutUs />
     </div>
   );
 };
